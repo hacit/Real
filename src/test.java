6f297d5589721e03401f6e181a0c4f83c3338e71
hacit <hacit.hh@gmail.com>
2016-04-27 16:05:18 +0900

    wwwwww

--- src/test.java
+++ src/test.java
@@ -3,11 +3,11 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("WTFtttttsadfaddfull?");
-=======
+//<<<<<<< HEAD
+		System.out.println("WTFtttttsadfaddfull?change?");
+//=======
 		System.out.println("branch01?");
->>>>>>> branch01
+//>>>>>>> branch01
 	}
 
 }
